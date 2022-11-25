a6d118257e5d94c3047359b0ecc82eb12e912d60
ardisyahputra12 <ardisaputraa12@gmail.com>
2022-11-25 11:40:40 +0700

    Create server

--- app.js
+++ app.js
@@ -1,2 +1,11 @@
-console.log("Hello World\n");
-console.log("Hello Again!\n");
+import express from 'express';
+
+let app = express();
+
+app.get('/', (req, res)=>{
+    res.send("Hello World!");
+})
+
+app.listen(8080, ()=>{
+    console.log("app is running!");
+})
